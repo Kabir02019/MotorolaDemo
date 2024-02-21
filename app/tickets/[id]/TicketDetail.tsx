cb2cd4d924a3040fe8a2d1b7f418424564ac8923
Kabir Khanna <kabirkhanna@Kabirs-MacBook-Pro.local>
2024-02-21 02:35:34 -0500

    delete functionality added

--- app/tickets/[id]/TicketDetail.tsx
+++ app/tickets/[id]/TicketDetail.tsx
@@ -13,6 +13,7 @@ import { TicketPriority } from '@/components/TicketPriority';
 import Link from 'next/link'
 import { buttonVariants } from '@/components/ui/button';
 import ReactMarkdown from 'react-markdown';
+import DeleteButton from './DeleteButton';
 
 
 interface Props{
@@ -59,9 +60,7 @@ const TicketDetail = ({ticket}: Props) => {
   variant: "default"
 })}`}>Edit Ticket</Link>
 
-<Link href= {`/tickets/edit/${ticket.id}`} className = {`${buttonVariants({
-  variant: "default"
-})}`}>Delete Ticket</Link>
+<DeleteButton ticketId = {ticket.id}/>
 
 </div>
 </div>
